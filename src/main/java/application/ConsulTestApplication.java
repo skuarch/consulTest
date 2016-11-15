8198449f243741f41469427a021ef8cd1defd086
skuarch <skuarch@yahoo.com.mx>
2016-11-14 22:56:06 -0600

    first commit

---
+++ src/main/java/application/ConsulTestApplication.java
@@ -0,0 +1,15 @@
+package application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ConsulTestApplication {
+
+        //======================================================================
+	public static void main(String[] args) {
+		SpringApplication.run(ConsulTestApplication.class, args);
+	}
+}
